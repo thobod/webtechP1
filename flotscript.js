fa1d725f43993e1e222e4707f587f77de10c8c6d
Unknown <t.boddeus@gmail.com>
2018-03-14 22:19:30 +0100

    Auto stash before merge of "master" and "origin/master"

---
+++ flotscript.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#ybutton").bind("click", onButtonClick);
+});
+
+function onButtonClick() {
+    var y = document.getElementsByName('yvalue');
+    y = y++;
+    $('p').html("yvalue", y);
+}
