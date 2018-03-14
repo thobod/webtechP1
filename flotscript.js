62333bd000416814cd28996731ee5f05c7293d91
Unknown <t.boddeus@gmail.com>
2018-03-14 22:31:14 +0100

    .

--- flotscript.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    $("#ybutton").bind("click", onButtonClick);
-});
-
-function onButtonClick() {
-    var y = document.getElementsByName('yvalue');
-    y = y++;
-    $('p').html("yvalue", y);
-}
