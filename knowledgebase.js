90a0395401cad59e5b9ed8bbe3ecbc7f34cf7f96
Unknown <t.boddeus@gmail.com>
2018-03-14 16:37:49 +0100

    sadjk

--- knowledgebase.js
+++ knowledgebase.js
@@ -17,9 +17,9 @@ class ReadMore {
 }
 
 home = new ReadMore("home");
-home.text = "Dit is een sammenvatting van de pagina home";
+home.text = "<hr> Welcome to the course. Internet is the most relevant information technology of our age. World Wide Web is its most important application. In this course we will learn the architecture of WWW, its main protocols and representation formats. The practical component of the course will focus on design and implementation of Web applications. Additional topics such as Semantic Web, Social Web and Adaptive Web will help us better undestand current trends of Web development.";
 grading = new ReadMore("grading");
-grading.text = "Dit is een sammenvatting van de pagina grading";
+grading.text =   "<hr/><p>50% of the course grade you will earn individually at exams; another 50% - by completing group assignments. There will be two exams:</p><ul><li>Midterm (20%)</li><li>Final(30%)</li></ul><p>..and three programming assignments:</p><ul><li>Assignment 1 (5%)</li><li>Assignment 2 (15%)</li><li>Assignment 3 (30%)</li></ul><p>You can retake the both exams at once during on the official retake date - 06/07/18. You cannot keep only a part of the retake grade. If you retake the exam, its entire grade will be used as 50% of your course grade. You cannot retake the assignments.</p><p>If you want to get the best possible grade, treat the deadline for assignments as strict. A day ends at 23:59:59. If your submission is a second late, it is a day late. You still receive a grade for a late assignment, but for every extra day you lose 20% of the grade. Hence, after the late day #5 there is no point in submitting. Any assignment submitted later will not be graded.</p><hr/>";
 schedule = new ReadMore("schedule");
 schedule.text = "Dit is een sammenvatting van de pagina schedule";
 tools = new ReadMore("tools");
